fa1054627ed286271d5ba8e0f9158086f9c306e1
Connor Smith <cjs.smith1993@gmail.com>
2013-04-13 14:41:34 -0600

    Graph.h updated

--- Graph.h
+++ Graph.h
@@ -10,12 +10,26 @@
 #define __Project5__Graph__
 
 #include <iostream>
+#include <vector>
+#include <list>
+#include <stack>
+#include <map>
+
+#include "Rule.h"
 
 class Graph {
     
 public:
+    std::vector<std::list<std::string> > adjList;
+    std::map<std::string, int> postNums;
     
-    
+    Graph();
+    Graph(std::vector<Rule*> rules);
+    ~Graph();
+    void createAdjList();
+    void traverse();
+    bool detectCycles();
+
 };
 
 #endif /* defined(__Project5__Graph__) */
